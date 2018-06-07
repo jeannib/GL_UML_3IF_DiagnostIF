706f7a68e91ae7f7ccfe5d0efe6d0b1257c4863a
Rouille Elise <erouille@if501-208-04.insa-lyon.fr>
2018-06-07 22:16:08 +0200

    affichage amélioré

--- main.cpp
+++ main.cpp
@@ -2,19 +2,26 @@
 #include <string>
 #include <fstream>
 #include "Analyseur.h"
-#include <sstream>
+#include <algorithm>
 
 using namespace std;
 
 string USER=""; 
 
-void menu();
+void menu(Analyseur a);
+
+bool handleCinFails();
+
+void chargerFichierRef(Analyseur * a);
 
 int main()
 {
 	bool connection = false;
 	string id, mdp;
-	cout << "Bienvenue sur l'application Diagnost'IF !" << endl;
+	
+	char esc_char = 27; // the decimal code for escape character is 27
+
+	cout << "\r\nBienvenue sur l'application"<<esc_char << "[1;31m" << " Diagnost'IF !\r\n" << esc_char << "[0m"  << endl;
 	
 	while(connection!=true){
 		cout << "Identifiant" << endl;
@@ -29,9 +36,11 @@ int main()
 			if(line == mdp)
 			{
 				connection=true;
-				cout << "Connecte avec succes" << endl;
+				cout << "Connecte avec succes\r\n" << endl;
 				USER = id;
-				menu();
+				Analyseur analyseur;
+				chargerFichierRef(&analyseur);
+				menu(analyseur);
 			}
 			else
 			{
@@ -42,67 +51,76 @@ int main()
 		{
 			cout << "Connexion echouee (identifiant non reconnu), veuillez reessayer" << endl;
 		}
-    
 	}
 	return 0;
 }
 
-int getInt()
+bool handleCinFails()
+// Algorithme :
+// Si on cin autre chose qu'un int, gère l'erreur pour eviter une boucle infinie
 {
-    int x=0;
-    bool loop=true;
-    while(loop)
-    {
-        std::string s;
-        std::getline(std::cin, s);
-
-        std::stringstream stream(s);
-        if(stream >> x)
-        {
-            loop=false;
-            continue;
-        }
-        std::cout << "Invalid!" << std::endl;
+    if(cin.fail()){
+        cout << "Entrez un nombre !\r\n";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        return true;
     }
-    return x;
-}
+    return false;
+} //----- Fin de HandleCinFails
 
-void menu() {
-	int choix;
-	Analyseur analyseur;
+void chargerFichierRef(Analyseur * analyseur)
+{
 	ifstream ficConf;
-	do {
-		cout << "1 -  Charger un fichier de référence" << endl;
-		cout << "2 -  Dépistage général d'empreinte(s)" << endl;
-		cout << "3 -  Dépistage spécifique d'empreinte(s)" << endl;
-		cout << "4 -  Afficher la liste des maladies" << endl;
-		cout << "5 -  Afficher l'historique" << endl;
-		cout << "6 -  Se déconnecter" << endl;
-		choix = getInt();
-		switch (choix) {
-		case 1 :
+	string strRef;
+	string strConf;
+	cout << "Fichier de reference" << endl;
+	cin >> strRef;
+	ifstream ficRef(strRef);
+	if (ficRef.is_open())
+	{
+		cout << "Fichier de configuration" << endl;
+		cin >> strConf;
+		ficConf.open(strConf);
+		if (ficConf.is_open())
 		{
-			string strRef;
-			string strConf;
-			cout << "Fichier de référence" << endl;
-			cin >> strRef;
-			ifstream ficRef(strRef);
-			if (ficRef.is_open())
+			analyseur->chargerBD(ficRef, ficConf);
+		}
+	}
+}
+void menu(Analyseur a) {
+	int choix=0;
+	
+	Analyseur analyseur = a;
+	cout << "\r\nVous utilisez le logiciel Diagnost'IF \r\n\r\nSelectionnez l'action que vous souhaitez effectuer puis tapez sur entree pour valider votre choix!"<<endl;
+	do {
+
+		cout << "\r\n1 -  Depistage general d'empreinte(s)" << endl;
+		cout << "2 -  Depistage specifique d'empreinte(s)" << endl;
+		cout << "3 -  Afficher la liste des maladies" << endl;
+		cout<< "4 -  Afficher les caracteristiques d'une maladie" << endl;
+		cout << "5 -  Afficher l'historique" << endl;
+		cout << "6 -  Charger un fichier de reference" << endl;
+		cout << "7 -  Se deconnecter" << endl;
+		
+		while(choix<1 || choix>7)
+		{
+			do{
+				cin >> choix;
+			}while(handleCinFails());
+			if(choix<1 || choix>7) 
 			{
-				cout << "Fichier de configuration" << endl;
-				cin >> strConf;
-				ficConf.open(strConf);
-				if (ficConf.is_open())
-				{
-					analyseur.chargerBD(ficRef, ficConf);
-				}
+				cout<<"Entrez une valeur entre 1 et 7 !"<<endl;
 			}
 		}
+		
+		cout << endl;
+		
+		switch (choix) {
+		
+		case 1:
+			cout << "1" << endl;
 			break;
 		case 2:
-			cout << "2" << endl;
-			break;
-		case 3:
 		{
 			string emp;
 			string mal;
@@ -122,23 +140,28 @@ void menu() {
 						analyseur.depistageSpecifique(empreinte, m);
 					}
 				}else{
-					cout << "Maladie introuvable dans nos données" << endl;
+					cout << "Maladie introuvable dans nos donnees" << endl;
 				}
+			}else{
+				cout<< "Empreinte introuvable\r\n"<<endl;
 			}
-			
 		}
 			break;
 			
-		case 4:
+		case 3:
 			analyseur.afficherMaladies();
 			break;
 		case 6 :
+			chargerFichierRef(&analyseur);
 			break;
-		default : 
-			cout<<"Entrez une valeur entre 1 et 6 !"<<endl;
-            break;
+		default :
+			break;
 		}
-	} while (choix != 6);
+		if(choix!=7){
+			choix=0;
+		}
+	} while (choix != 7);
+	
 }
 
 
